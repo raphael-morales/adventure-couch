b379dc7c184398792bd408b949806bf8d02202b5
Guillaume CAVALIE <actix2013@gmail.com>
2020-04-30 04:10:43 +0200

    WEBCAMCONTROLER LEVEL FUSION

--- src/Controller/WebcamController.php
+++ src/Controller/WebcamController.php
@@ -45,14 +45,43 @@ class WebcamController extends AbstractController
         category=city/orderby=popularity,desc/limit=3?key=7SWJ1kS5vuhNx8oNB2Fv3zTLH2uS2PvM");
         $webcams = $response->toArray();
 
-        return $this->twig->render('Home/index.html.twig', ['webcams'=>$webcams['result']['webcams']]);
+        return $this->twig->render('Home/index.html.twig', ['webcams' => $webcams['result']['webcams']]);
     }
 
     public function level($id)
     {
         $questionManager = new QuestionManager();
-        $level =  $questionManager->selectQuestionsByLevel($id);
+        $level = $questionManager->selectQuestionsByLevel($id);
+        $webcams = $this->getWebcams($id);
 
-        return $this->twig->render('Home/index.html.twig', ['level' => $level]);
+        // ----------------------------------
+        $pseudo = '';
+        if (!empty($_POST)) {
+            $pseudo = trim($_POST['pseudo']);
+        }
+        $couch = new Couch(2, 2);
+        $couchPosition = $couch->getPosition();
+        //------------------------------------------------------------
+
+        return $this->twig->render('Home/index.html.twig', ['level' => $level, 'webcams' =>
+            $webcams['result']['webcams'], 'couchPosition' => $couchPosition, 'pseudo' => $pseudo]);
+    }
+
+
+    public function getWebcams(string $country)
+    {
+        $realCountry = "FR";
+        if ($country === "1") {
+            $realCountry = "FR";
+        } elseif ($country === "2") {
+            $realCountry = "AE";
+        } elseif ($country === "3") {
+            $realCountry = "JP";
+        }
+        $client = HttpClient::create();
+        $response = $client->request('GET', "https://api.windy.com/api/webcams/v2/list/country=$realCountry/
+        category=city/orderby=popularity,desc/limit=3?key=7SWJ1kS5vuhNx8oNB2Fv3zTLH2uS2PvM");
+        $webcams = $response->toArray();
+        return $webcams;
     }
 }
